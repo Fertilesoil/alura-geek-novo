811f894b71b9d835af70acf48f89615d4d17e2be
Fertilesoil <gasnbloop@gmail.com>
2024-07-10 15:20:43 -0300

    Refatoração de métodos da CardService

--- Eventos/Escutadores.js
+++ Eventos/Escutadores.js
@@ -4,14 +4,14 @@ import Produto from "../Modelos/Produto";
 
 const produto = new Produto("", "", "");
 
-const campos = Object.entries(Componentes).splice(0, 3);
+const campos = Object.values(Componentes).splice(0, 3);
 
 Componentes.botaoEnviar.onclick = async (evento) => {
   evento.preventDefault();
   await CardService.criarCard(produto);
-  campos.forEach(campo => (campo ? campo[1].value = "" : campo))
+  campos.forEach(campo => campo.value = "")
 }
 
 campos.forEach(campo => {
-  return campo[1].addEventListener("input", (evento) => produto.popularProduto(evento))
+  return campo.addEventListener("input", (evento) => produto.popularProduto(evento))
 });
--- Service/CardService.js
+++ Service/CardService.js
@@ -4,47 +4,84 @@ import Componentes from "../Componentes/Componentes";
 import Card from "../Modelos/Card";
 
 class CardService {
+  #produtos
+
+  constructor() {
+    this.#produtos = [];
+  }
+
+  get produtos() {
+    return this.#produtos;
+  }
+
+  adicionarProduto(produto) {
+    this.#produtos.push(produto);
+    this.mostrarCards("atualizar-card");
+  }
+
   apagarCard = async (id) => {
-    const options = { method: "DELETE" }
-    await fetch(`http://localhost:3000/produtos/${id}`, options)
-    Componentes.cardsContainer.innerHTML = "";
-    this.mostrarCards();
+    const options = { method: "DELETE" };
+    await fetch(`http://localhost:3000/produtos/${id}`, options);
+    const novoArray = this.#produtos.filter(produto => produto.id !== id);
+    this.#produtos = novoArray;
+    this.mostrarCards("atualizar-card");
   }
 
-  mostrarCards = async () => {
-    const produtos = await Api.listarProdutos();
+  mostrarCards = async (atualizar) => {
+    if (atualizar === "atualizar-card") {
+      Componentes.cardsContainer.innerHTML = "";
 
-    produtos.forEach(produto => {
-      const novoCard = new Card(produto.nome, produto.preco, produto.imagem, produto.id);
-      return Componentes.cardsContainer.appendChild(novoCard.render())
-    });
-
-    const botoesDeletar = document.querySelectorAll('.botao_delete');
-    botoesDeletar.forEach(botao => {
-      botao.addEventListener('click', (evento) => {
-        const produtoId = evento.currentTarget.getAttribute('data-id');
-        this.apagarCard(produtoId);
+      this.#produtos.forEach(produto => {
+        const novoCard = new Card(produto.nome, produto.preco, produto.imagem, produto.id);
+        return Componentes.cardsContainer.appendChild(novoCard.render())
       });
-    });
+
+      const botoesDeletar = document.querySelectorAll('.botao_delete');
+      botoesDeletar.forEach(botao => {
+        botao.addEventListener('click', (evento) => {
+          const produtoId = evento.currentTarget.getAttribute('data-id');
+          this.apagarCard(produtoId);
+        });
+      });
+    } else {
+      const produtos = await Api.listarProdutos();
+
+      this.#produtos = produtos;
+
+      Componentes.cardsContainer.innerHTML = "";
+
+      this.#produtos.forEach(produto => {
+        const novoCard = new Card(produto.nome, produto.preco, produto.imagem, produto.id);
+        return Componentes.cardsContainer.appendChild(novoCard.render())
+      });
+
+      const botoesDeletar = document.querySelectorAll('.botao_delete');
+      botoesDeletar.forEach(botao => {
+        botao.addEventListener('click', (evento) => {
+          const produtoId = evento.currentTarget.getAttribute('data-id');
+          this.apagarCard(produtoId);
+        });
+      });
+    }
   }
 
-  criarCard = async ({ nome, preco, imagem, id }) => {
+  criarCard = async (produto) => {
+    const novoProduto = {
+      nome: produto.nome,
+      preco: produto.preco,
+      imagem: produto.imagem,
+      id: v4()
+    }
 
     await fetch("http://localhost:3000/produtos", {
       method: "POST",
       headers: {
         "Content-type": "application/json"
       },
-      body: JSON.stringify({
-        nome,
-        preco,
-        imagem,
-        id: v4()
-      })
+      body: JSON.stringify(novoProduto)
     })
 
-    Componentes.cardsContainer.innerHTML = "";
-    this.mostrarCards();
+    this.adicionarProduto(novoProduto);
   }
 }
 
